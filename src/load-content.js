c727390c33e1e8e236865b7c4519337d0bc7b78f
Veron Chetty <veronfc1701@gmail.com>
2023-09-19 21:43:36 +0200

    split content into modules, more styling

--- src/load-content.js
+++ src/load-content.js
@@ -1,7 +1,7 @@
+import {loadMain} from './main-content.js'
 import './style.css'
-import si from './img/pexels-guilherme-rossi-1819669.jpg'
+import si from './img/pexels-volkan-vardar-3887985.jpg'
 import hi from './img/absurd.design-chapter1-31.png'
-import content from './content.xml'
 
 const header = document.createElement('div')
 header.classList.add('header')
@@ -10,10 +10,6 @@ const headImg = new Image()
 headImg.src = hi
 headImg.className = 'head'
 header.appendChild(headImg)
-
-const main = document.createElement('div')
-main.className = 'main'
-main.innerHTML = '<div class="description"><strong>That Food Place</strong> is an upscale and sophisticated dining establishment that sets the standard for exquisite culinary experiences. Nestled in the heart of a bustling city, this restaurant is a haven for food enthusiasts seeking a memorable gastronomic adventure</div>'
 
 const side = document.createElement('div')
 side.className = 'side'
@@ -25,11 +21,11 @@ side.appendChild(sideImg)
 
 const curveLeft = document.createElement('div')
 curveLeft.className = 'curve-left'
-const curveRight = document.createElement('dive')
+const curveRight = document.createElement('div')
 curveRight.className = 'curve-right'
 
 document.body.appendChild(header)
-document.body.appendChild(main)
+document.body.appendChild(loadMain())
 document.body.appendChild(side)
 document.body.appendChild(curveLeft)
 document.body.appendChild(curveRight)
