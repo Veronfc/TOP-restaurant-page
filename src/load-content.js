ef74fb4e86dcef3119be9ea76b47acddc95ac939
Veronfc <veronfc1701@gmail.com>
2023-09-19 08:19:59 +0200

    change some styling

--- src/load-content.js
+++ src/load-content.js
@@ -3,7 +3,7 @@ import si from './img/pexels-lumn-604969.jpg'
 
 const header = document.createElement('div')
 header.classList.add('header')
-header.innerHTML = '<i></i>'
+header.innerHTML = '<em>restaurant name</em>'
 
 const main = document.createElement('div')
 main.classList.add('main')
@@ -16,6 +16,13 @@ sideImg.src = si
 sideImg.classList.add('side-image')
 side.appendChild(sideImg)
 
+const curveLeft = document.createElement('div')
+curveLeft.className = 'curve-left'
+const curveRight = document.createElement('dive')
+curveRight.className = 'curve-right'
+
 document.body.appendChild(header)
 document.body.appendChild(main)
 document.body.appendChild(side)
+document.body.appendChild(curveLeft)
+document.body.appendChild(curveRight)
