9d09dcd95f6d0c5b85138c9d6bff4dd145c419ee
Veronfc <veronfc1701@gmail.com>
2023-09-20 09:42:12 +0200

    add more styling and content

--- src/main-content.js
+++ src/main-content.js
@@ -4,7 +4,7 @@ function loadMain() {
 
   const desc = document.createElement('div')
   desc.className = 'desc'
-  desc.innerHTML = 'That Food Place is an upscale and sophisticated dining establishment that sets the standard for exquisite culinary experiences. Nestled in the heart of a bustling city, this restaurant is a haven for food enthusiasts seeking a memorable gastronomic adventure'
+  desc.innerHTML = 'That Food Place is an upscale and sophisticated dining establishment that sets the standard for exquisite culinary experiences. Nestled in the heart of a bustling city, this restaurant is a haven for food enthusiasts seeking a memorable gastronomic adventure.'
 
   const ambiance = document.createElement('div')
   ambiance.className = 'desc'
@@ -14,11 +14,23 @@ function loadMain() {
   cuisine.className = 'desc'
   cuisine.innerHTML = '<strong>CUISINE</strong>The culinary team at That Food Place is renowned for its creativity and dedication to delivering an extraordinary dining experience. The menu is a carefully curated selection of globally inspired dishes, crafted with precision and artistry. From the freshest seafood to prime cuts of meat, every ingredient is meticulously sourced to ensure the highest quality. Whether you\'re a seafood aficionado, a carnivore, or a vegetarian, That Food Place has something to delight your palate.'
 
+  const selection = document.createElement('div')
+  selection.className = 'desc'
+  selection.innerHTML = '<strong>WINE & BEVERAGE SELECTION</strong>The wine cellar at That Food Place is extensive, boasting an impressive collection of both vintage and contemporary wines from around the world. The sommelier is ready to assist guests in selecting the perfect wine to complement their meal. The restaurant also offers a range of craft cocktails, artisanal spirits, and non-alcoholic options for those seeking alternative beverages.'
+
+  const signature = document.createElement('div')
+  signature.className = 'signature'
+  signature.innerHTML = '<strong>SIGNATURE DISHES</strong><div class=dish><strong>Truffle-infused Lobster Risotto</strong>Creamy Arborio rice cooked to perfection, infused with the earthy aroma of black truffles, and adorned with succulent lobster meat</div><div class=dish><strong>Filet Mignon Rossini</strong>A tender, melt-in-your-mouth filet mignon topped with seared foie gras, drizzled with a rich truffle demi-glace, and served with truffle mashed potatoes</div><div class=dish><strong>Vegetable Garden Symphony</strong>A vibrant and artistic presentation of seasonal vegetables and herbs, celebrating the colors and flavors of the garden</div><div class=dish><strong>Chocolate Decadence</strong>A sumptuous dessert that combines layers of dark and white chocolate mousse, garnished with edible gold leaf and fresh berries</div>'
+
   main.appendChild(desc)
   main.appendChild(document.createElement('hr'))
   main.appendChild(ambiance)
   main.appendChild(document.createElement('hr'))
   main.appendChild(cuisine)
+  main.appendChild(document.createElement('hr'))
+  main.appendChild(selection)
+  main.appendChild(document.createElement('hr'))
+  main.appendChild(signature)
 
   return main
 }
